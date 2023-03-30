44f67dc810579789cd05d27428022d0a2b88e43f
Matheus Yelgde <yelgde.8@gmail.com>
2023-03-30 18:51:06 -0300

    Lint

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 export default class Title extends Component {
   render() {
+    // eslint-disable-next-line react/prop-types
     const { headline } = this.props;
-
     return (
       <section>
         <h2>{headline}</h2>
