ddbbe2630bbaa247bbf6f716cf5d43571670affc
Matheus Yelgde <yelgde.8@gmail.com>
2023-03-30 15:50:00 -0300

    Requisito 7 Feito

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import planets from '../data/planets';
 
 export default class PlanetCard extends Component {
   render() {
     const { planetName, planetImage} = this.props;
+    const { name } = planets;
+    const { image } = planets;
 
     return (
       <div data-testid="planet-card">
         <p data-testid="planet-name">{ planetName }</p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        {/* <ul>
+          {planets.map((system) =>
+            <li key={ name }> { system } </li>)}
+        </ul> */}
       </div>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 export default class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <PlanetCard />
       </div>
     );
   }
